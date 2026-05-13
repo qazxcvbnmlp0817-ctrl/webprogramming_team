dc7dd4afa8f2f4f7325225652492cb55731e989c
lTvTl <wlstjd0124@mokpo.ac.kr>
2026-05-13 17:06:55 +0900

    feat: add SchoolDto, FacultyDto, DeptSelectionDto

---
+++ demo/demo/src/main/java/com/example/demo/dto/SchoolDto.java
@@ -0,0 +1,22 @@
+package com.example.demo.dto;
+
+import java.util.List;
+
+public class SchoolDto {
+    private Long id;
+    private String name;
+    private String description;
+    private List<FacultyDto> faculties;
+
+    public SchoolDto(Long id, String name, String description, List<FacultyDto> faculties) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.faculties = faculties;
+    }
+
+    public Long getId()                   { return id; }
+    public String getName()               { return name; }
+    public String getDescription()        { return description; }
+    public List<FacultyDto> getFaculties() { return faculties; }
+}
